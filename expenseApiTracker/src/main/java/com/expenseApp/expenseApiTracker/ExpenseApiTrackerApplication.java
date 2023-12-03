73adcc754075862d63228c7d61bbd0a611891453
Dipak122-debug <dipak628das@gmail.com>
2023-12-04 00:03:04 +0530

    04/12/2023 --commit

---
+++ expenseApiTracker/src/main/java/com/expenseApp/expenseApiTracker/ExpenseApiTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.expenseApp.expenseApiTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseApiTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseApiTrackerApplication.class, args);
+	}
+
+}
---
+++ expenseApiTracker/src/test/java/com/expenseApp/expenseApiTracker/ExpenseApiTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenseApp.expenseApiTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseApiTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
